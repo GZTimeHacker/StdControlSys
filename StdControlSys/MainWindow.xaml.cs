d0168939358c961defe894132899a1f234af6edf
GZTimeHacker <du_du_waw@163.com>
2018-03-31 17:19:34 +0800

    V0.0.1
    核心数据格式完成

--- StdControlSys/MainWindow.xaml.cs
+++ StdControlSys/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace StdControlSys
 {
@@ -20,9 +21,47 @@ namespace StdControlSys
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random random = new Random();
+        List<Std> stds = new List<Std>();
+        List<Group> groups = new List<Group>();
         public MainWindow()
         {
             InitializeComponent();
+            ReadFileData("GroupInfo.xml", "StdInfo_GY4.xml");
         }
+
+        /// <summary>
+        /// 读取文件
+        /// </summary>
+        /// <param name="GroupFileName">组文件位置</param>
+        /// <param name="StdFileName">学生文件位置</param>
+        public void ReadFileData(string GroupFileName,string StdFileName)
+        {
+            stds = null;
+            groups = null;
+            groups = new List<Group>();
+            #region 写入测试
+            //List<GroupInfo> ingroups = new List<GroupInfo>();
+            //ingroups.Add(new GroupInfo("第一组", 1, 170401, new int[] { 170402, 170403, 170404, 170405, 170406, 170407 }));
+            //ingroups.Add(new GroupInfo("SiO2", 2, 170411, new int[] { 170412, 170413, 170414, 170415, 170416, 170417 }));
+            //ingroups.Add(new GroupInfo("如来佛祖", 3, 170421, new int[] { 170422, 170423, 170424, 170425, 170426, 170427 }));
+            //if (Info.SerializerGrp(ingroups, "GroupInfo.xml")) MessageBox.Show("Done");
+            #endregion
+            List<GroupInfo> groupinfos;
+            groupinfos = Info.DeserializeGrp_List(GroupFileName);
+            stds = Info.DeserializeStd_List(StdFileName);
+            foreach (var item in groupinfos)
+            {
+                groups.Add(new Group(stds, item));
+            }
+            /* 
+             * foreach (var item in groups)
+             * {
+             *     MessageBox.Show(item.ToString());
+             * }
+             */
+            groupinfos = null;
+        }
+
     }
 }
---
+++ StdControlSys/StdInfo.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+using System.Windows;
+
+namespace StdControlSys
+{
+    /// <summary>
+    /// 学生基本信息对象
+    /// </summary>
+    public class Std
+    {
+        /// <summary>
+        /// 班级
+        /// </summary>
+        public int Class { get; set; }
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 学号
+        /// </summary>
+        public int Number { get; set; }
+        /// <summary>
+        /// 新建学生对象
+        /// </summary>
+        public Std()
+        {
+            Class = 0;
+            Name = "null";
+            Number = 0;
+        }
+        /// <summary>
+        /// 新建学生对象
+        /// </summary>
+        /// <param name="Class">班级</param>
+        /// <param name="Name">姓名</param>
+        /// <param name="Number">学号</param>
+        public Std(int Class,string Name,int Number)
+        {
+            this.Class = Class;
+            this.Name = Name;
+            this.Number = Number;
+        }
+        public override string ToString()
+        {
+            return "班级：高一(" + Class + ")班" +
+                "\n学号：" + Number +
+                "\n姓名：" + Name;
+        }
+    }
+
+    /// <summary>
+    /// 小组对象
+    /// </summary>
+    public class Group
+    {
+        /// <summary>
+        /// 组员
+        /// </summary>
+        public List<Std> Members;
+        /// <summary>
+        /// 组长
+        /// </summary>
+        public Std Leader;
+        /// <summary>
+        /// 组名
+        /// </summary>
+        public string Name;
+        /// <summary>
+        /// 小组编号
+        /// </summary>
+       public int Order;
+        /// <summary>
+        /// 实例化小组
+        /// </summary>
+        /// <param name="stds">学生列表</param>
+        /// <param name="info">小组信息</param>
+        public Group(List<Std> stds,GroupInfo info)
+        {
+            Members = new List<Std>();
+            Name = info.Name;
+            Order = info.Order;
+            Leader = stds.Find(s => s.Number == info.LeaderNum);
+            foreach (int n in info.MembersNum)
+            {
+                Members.Add(stds.Find(s => s.Number == n));
+            }
+        }
+        public override string ToString()
+        {
+            string str="";
+            str += Name + "\t[" + Order + "]\n";
+            str += "-=组长=-\n" + Leader.Name + "\n-=组员=-\n";
+            foreach (Std s in Members)
+            {
+                str += s.Name + "\t";
+            }
+            str += "\n";
+            return str;
+        }
+    }
+
+    /// <summary>
+    /// 小组信息对象
+    /// </summary>
+    public class GroupInfo
+    {
+        /// <summary>
+        /// 小组名称
+        /// </summary>
+        public string Name;
+        /// <summary>
+        /// 小组序号
+        /// </summary>
+        public int Order;
+        /// <summary>
+        /// 组长学号
+        /// </summary>
+        public int LeaderNum;
+        /// <summary>
+        /// 组员学号
+        /// </summary>
+        public int[] MembersNum;
+        /// <summary>
+        /// 新建小组信息对象
+        /// </summary>
+        public GroupInfo()
+        {
+            Name = "";
+            Order = -1;
+            LeaderNum = 0;
+            MembersNum = null;
+        }
+        /// <summary>
+        /// 新建小组信息对象
+        /// </summary>
+        /// <param name="Name">小组名称</param>
+        /// <param name="Order">小组序号</param>
+        /// <param name="LeaderNum">组长学号</param>
+        /// <param name="MembersNum">组员学号数组</param>
+        public GroupInfo(string Name,int Order, int LeaderNum,int[] MembersNum)
+        {
+            this.Name = Name;
+            this.Order = Order;
+            this.LeaderNum = LeaderNum;
+            this.MembersNum = MembersNum;
+        }
+        public override string ToString()
+        {
+            string str = "";
+            str += Name + "\t[" + Order + "]\n";
+            str += "-=组长=-\n" + LeaderNum+ "\n-=组员=-\n";
+            foreach (int s in MembersNum)
+            {
+                str += s + "\t";
+            }
+            str += "\n";
+            return str;
+        }
+    }
+
+    /// <summary>
+    /// 读取与写入学生和班级信息
+    /// </summary>
+    public class Info
+    {
+        /// <summary>
+        /// 序列化StdInfo数组
+        /// </summary>
+        /// <param name="stds">学生信息数组</param>
+        /// <param name="FileName">文件位置</param>
+        /// <returns>序列化是否成功</returns>
+        static public bool SerializerStd(Std[] stds,string FileName)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Std[]));
+            using(FileStream fs = new FileStream(FileName, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                try
+                {
+                    xmlSerializer.Serialize(fs, stds);
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "错误信息", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return false;
+                }
+            }
+        }
+        /// <summary>
+        /// 序列化StdInfo数组(List)
+        /// </summary>
+        /// <param name="stds">学生信息数组</param>
+        /// <param name="FileName">文件位置</param>
+        /// <returns>序列化是否成功</returns>
+        static public bool SerializerStd(List<Std> stds, string FileName)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Std>));
+            using (FileStream fs = new FileStream(FileName, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                try
+                {
+                    xmlSerializer.Serialize(fs, stds);
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "错误信息", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return false;
+                }
+            }
+        }
+        /// <summary>
+        /// 反序列化StdInfo数组
+        /// </summary>
+        /// <returns>学生信息数组</returns>
+        static public Std[] DeserializeStd(string FileName)
+        {
+            Std[] stds;
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Std[]));
+            using(FileStream fs = new FileStream(FileName, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                try
+                {
+                    stds = (Std[])xmlSerializer.Deserialize(fs);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message,"错误信息",MessageBoxButton.OK,MessageBoxImage.Error);
+                }
+            }
+            return null;
+        }
+        /// <summary>
+        /// 反序列化StdInfo数组(List)
+        /// </summary>
+        /// <returns>学生信息列表</returns>
+        static public List<Std> DeserializeStd_List(string FileName)
+        {
+            List<Std> stds;
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Std>));
+            using (FileStream fs = new FileStream(FileName, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                try
+                {
+                    stds = (List<Std>)xmlSerializer.Deserialize(fs);
+                    return stds;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "错误信息", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            return null;
+        }
+        /// <summary>
+        /// 序列化小组信息数组(List)
+        /// </summary>
+        /// <param name="groups">小组信息数组</param>
+        /// <param name="FileName">文件位置</param>
+        /// <returns>序列化是否成功</returns>
+        static public bool SerializerGrp(List<GroupInfo> groups,string FileName)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<GroupInfo>));
+            using (FileStream fs = new FileStream(FileName, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                try
+                {
+                    xmlSerializer.Serialize(fs, groups);
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "错误信息", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return false;
+                }
+            }
+        }
+        /// <summary>
+        /// 反序列化小组信息数组(List)
+        /// </summary>
+        /// <param name="FileName">文件位置</param>
+        /// <returns>小组信息列表</returns>
+        static public List<GroupInfo> DeserializeGrp_List(string FileName)
+        {
+            List<GroupInfo> groups;
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<GroupInfo>));
+            using (FileStream fs = new FileStream(FileName, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                try
+                {
+                    groups = (List<GroupInfo>)xmlSerializer.Deserialize(fs);
+                    return groups;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "错误信息", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            return null;
+        }
+    }
+}
